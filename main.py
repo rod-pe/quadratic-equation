8ead7f308f8907f4b3c52de3ef3ec540a104fa71
Rodrigo <rep.rodpe2@outlook.com>
2025-11-27 17:17:05 +0000

    Solver for quadratic equations; doesn't handle complex roots.

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+a = int(input("Insira o primeiro valor: "))
+b = int(input("Insira o segundo valor: "))
+c = int(input("Insira o terceiro valor: "))
+
+def bhaskara(a, b, c):
+    delta = (b**2) -4*a*c
+
+    if delta < 0:
+        return "Delta negativo. Não é possível finalizar a equação."
+    elif delta == 0:
+        x = -b / (2*a)
+        return "Delta é 0. Raiz única: ", x
+    else:
+        x1 = (-b + math.sqrt(delta)) / (2*a)
+        x2 = (-b - math.sqrt(delta)) / (2*a)
+        return 'Delta: ', delta, '. As raízes são: ', x1, 'e ', x2
+    
+resultado = bhaskara(a, b, c)
+print(resultado)
